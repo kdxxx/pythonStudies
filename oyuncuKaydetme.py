db99fd3cf4966622541a73405185507b21608a18
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-03-03 23:42:06 +0300

    Create oyuncuKaydetme.py

---
+++ oyuncuKaydetme.py
@@ -0,0 +1,13 @@
+print ("Oyuncu Kaydetme Programı")
+
+ad = input("Oyuncunun adı: ")
+soyad = input("Oyuncunun soyadı: ")
+takım = input("Oyuncunun takımı: ")
+
+bilgiler = [ad,soyad,takım]
+
+print("Bilgiler Kaydediliyor.....")
+
+print("Oyuncunun adı: {}\n Oyuncunun soyadı: {}\n Oyuncunun takımı: {}\n".format(bilgiler[0],bilgiler[1],bilgiler[2]))
+
+print("Bilgiler Kaydedildi....")
