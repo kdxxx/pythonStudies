9babb33edd30d89ba2640208ca66c651ebb1b100
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-03-20 23:12:52 +0300

    Create dövizUygulaması.py

---
+++ dövizUygulaması.py
@@ -0,0 +1,18 @@
+import sys
+
+import requests
+
+url = "http://api.fixer.io/latest?base="
+
+birinci_doviz = input("Birinci döviz: ")
+ikinci_doviz = input("İkinci döviz: ")
+miktar = float(input("miktar: "))
+
+response = requests.get(url+birinci_doviz)
+json_verisi = response.json()
+
+try:
+    print(json_verisi["rates"][ikinci_doviz]*miktar)
+except KeyError:
+    sys.stderr.write("yanlış para birimi")
+    sys.stderr.flush()
