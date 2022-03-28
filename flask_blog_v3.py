44d28f18ede32a1472f862dc93dfcb84ed65d2fc
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-03-28 22:16:44 +0300

    Create flask_blog_v3.py

---
+++ flask_blog_v3.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    sayi = 10
+    sayi2 = 20
+
+    return render_template("index.html",number = sayi,number2=sayi2)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
