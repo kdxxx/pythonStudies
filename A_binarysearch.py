e50e28bc0a052cf9d5b4edfdf7759ed637c6f8c5
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-04-13 21:24:39 +0300

    Update A_binarysearch.py

--- A_binarysearch.py
+++ A_binarysearch.py
@@ -13,13 +13,13 @@ def sorted_array_to_bst(nums):
     node.left = sorted_array_to_bst(nums[:mid_val])
     node.right = sorted_array_to_bst(nums[mid_val+1:])
     return node
-
 def preOrder(node): 
     if not node: 
         return      
     print(node.val)
+    preOrder(node.right)
     preOrder(node.left) 
-    preOrder(node.right)   
+       
     
-result = sorted_array_to_bst([1, 2, 3, 4, 5, 6, 7])
+result = sorted_array_to_bst([1, 2, 3, 4, 5, 6,7])
 preOrder(result)
