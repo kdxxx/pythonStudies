a3fe20bce02ee11ccbdad46ef9a94bdab3ef00a7
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-04-27 23:16:48 +0300

    Update flask_dashboard_v21.py

--- flask_dashboard_v21.py
+++ flask_dashboard_v21.py
@@ -1,11 +1,12 @@
 {% extends "layout.html" %}
 
 {% block body %}
+
 <h3> Kontrol Paneli </h3>
 <small> Hoş Geldin ,{{session["username"]}}</small>
+
 <hr>
 
 <a href ="/addarticle" class = "btn btn-danger">Makale Ekle</a>
-
 
 {% endblock body %}
