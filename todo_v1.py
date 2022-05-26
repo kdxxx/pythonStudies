306ff62d542b28178c78db91f39b6fc91b7a3288
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-05-26 22:26:18 +0300

    Create todo_v1.py
    todo app with db browser. 
    Careful with the path.

---
+++ todo_v1.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/90546/Desktop/ToDoApp/todo.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    complete = db.Column(db.Boolean)
+
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
+    
