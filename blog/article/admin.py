3f7ce572f5cb00df655cd0c3611b77020686fe08
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-05-29 22:08:41 +0300

    Add files via upload

--- blog/article/admin.py
+++ blog/article/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Article
-admin.site.register(Article)
 
+# admin.site.register(Article)
+admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+
+    list_display = ["title","author","created_date"]
+    list_display_links = ["title","created_date"]
+
+    search_fields = ["title"]
+    list_filter = ["created_date"]
+    class Meta:
+        mdoel = Article
