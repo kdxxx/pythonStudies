5c4d6f054e40eb8e5bb40feec82e9e2999644db7
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-05-30 19:25:02 +0300

    Add files via upload

--- blog/blog/article/views.py
+++ blog/blog/article/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
+    context = {
+        "numbers":[1,2,3,4,5,6],
+    }
     #return HttpResponse("<h3>Ana Sayfa</h3>")
-    return render(request,"index.html")
+    return render(request,"index.html",context)
 
 def abou(request):
     return render(request="about.html")
