4e0903ce0e97c6cab05ab817540098e16724ca37
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-06-07 12:54:17 +0300

    Add files via upload

--- blog/blog/user/views.py
+++ blog/blog/user/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render,redirect
-from .forms import RegisterForm
+from .forms import LoginForm, RegisterForm
 from django.contrib import messages
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login,authenticate
 
 # Create your views here.
 def register(request):
@@ -58,7 +58,24 @@ def register(request):
 """
 
 def loginUser(request):
-    return render(request,"login.html")
+    form = LoginForm(request.POST or None)
+    context ={
+        "form":form
+    }
 
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+       
+        user =  authenticate(username=username,password=password)
+
+        if user is None:
+            messages.info(request,"Kullanıcı adı veya parola hatalı")
+            return render(request, "login.html",context)
+
+        messages.success(request,"Başarıla Girildi")
+        login(request,user)
+        return redirect("index")
+    return render(request,"login.html",context)
 def logoutUser(request):
     pass
